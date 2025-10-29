5fe103c0e2f3fa3cbdd5667d0f65544abea5d6be
ix0xid <Kostya.barnung@gmail.com>
2025-10-29 20:56:45 +0500

    nextcommit

--- users/forms.py
+++ users/forms.py
@@ -6,13 +6,10 @@ from users.models import User
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField()
     password = forms.CharField()
+
     class Meta:
         model = User
         fields = ('username', 'password')
 
-
-username = forms.CharField()
-
-password = forms.CharField()
 
 # А в разметке для инпутов добавляем атрибут name, в котором значениями будут username и password. И обязательно инпут и лэбел должны быть связаны через id, т.е у label должен быть атрибут for  в котором должно быть указано то же самое, что и в id input.
