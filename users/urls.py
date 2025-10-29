a291cc9b52cf77cbdc69a30d42deabd5887c5df9
ix0xid <Kostya.barnung@gmail.com>
2025-10-29 18:38:43 +0500

    nextcommit

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.template.context_processors import media
 from django.urls import path
-from users.views import login, register
+from .views import login_view, registerview
 
 app_name = 'users'
 urlpatterns = [
-    path('login/', login ,name='login'),
-    path('register/', register ,name='register'),
+    path('login/', login_view ,name='login'),
+    path('register/', registerview ,name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
 from .models import User
-from users.forms import UserLoginForm
+from django.db import IntegrityError
+from django.shortcuts import render, redirect, get_object_or_404,reverse
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from .forms import UserLoginForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login as auth_login
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
-def login(request):
-    context = {'form': UserLoginForm()}
+def login_view(request):
+    if request.method == 'POST':  # Изменено на POST
+        form = AuthenticationForm(data=request.POST)  # Используйте data=request.POST
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)  # Обратите внимание, что нужно передавать request
+                return redirect('common')  # Перенаправление на общую страницу
+            else:
+                return render(request, 'users/login.html', {'form': form, 'error': 'Неверные имя или пароль'})
+        else:
+            return render(request, 'users/login.html', {'form': form, 'error': 'Ошибка в форме'})
+    else:
+        form = AuthenticationForm()
+    context = {'form': form}
     return render(request, 'users/login.html',context)
-def register(request):
+def registerview(request):
     return render(request, 'users/register.html')
 
