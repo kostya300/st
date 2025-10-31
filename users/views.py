9fe7fd240b377aa030c453455c1283d57ec2e22f
ix0xid <Kostya.barnung@gmail.com>
2025-10-31 14:33:58 +0500

    nextcommit

--- users/views.py
+++ users/views.py
@@ -56,7 +56,7 @@ def registerview(request):
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
-# logutform
+# logutform1234
 
 def custom_logout(request):
     logout(request)  # стандартный выход
