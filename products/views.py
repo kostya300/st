6c86caf506fe8de3b6e35ad121bba2ea84e05bf8
ix0xid <Kostya.barnung@gmail.com>
2025-11-05 00:12:55 +0500

    nextcommit

--- products/views.py
+++ products/views.py
@@ -13,12 +13,7 @@ from django.db.models import Sum
 def common(request):
     return render(request, "products/common.html")
 def products(request,category_id=None):
-    if category_id:
-        category = ProductCategory.objects.get(id=category_id)
-        products = Product.objects.filter(category=category)
-    else:
-        products = Product.objects.all()
-    context = {'categories':ProductCategory.objects.all(),'products':products,}
+    context = {'categories':ProductCategory.objects.all(),'products':Product.objects.filter(category_id=category_id) if category_id else Product.objects.all(),}
     return render(request, "products/products.html",context)
 
 
