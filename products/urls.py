fbc3a4c99df59a6a7cf131233d6bd01433045617
ix0xid <Kostya.barnung@gmail.com>
2025-11-05 20:04:35 +0500

    nextcommit

--- products/urls.py
+++ products/urls.py
@@ -2,10 +2,12 @@
 
 from django.urls import path
 from . import views
+from .views import ProductListView
+
 app_name = 'products'
 urlpatterns = [
-    path('', views.products, name='catalog_general'),
-    path('category/<int:category_id>/', views.products, name='category_detail'),
+    path('', ProductListView.as_view(), name='catalog_general'),
+    path('category/<int:category_id>/', ProductListView.as_view(), name='category_detail'),
     path('baskets/add/<int:product_id>/',views.basket_add_product, name='basket_add'),
     path('basket/remove/<int:basket_id>/',views.basket_remove, name='basket_remove'),
 
--- products/views.py
+++ products/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from unicodedata import category
+
 from .models import Product, ProductCategory, Basket
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
@@ -8,19 +12,40 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from django.core.paginator import Paginator
 # Create your views here.
-def common(request):
-    return render(request, "products/common.html")
-def products(request,category_id=None):
-    if category_id:
-        products_list = Product.objects.filter(category_id=category_id)
-    else:
-        products_list = Product.objects.all()
-    paginator = Paginator(products_list, 2)  # 2 товара на страницу
-    page_number = request.GET.get('page')  # Получаем номер страницы из запроса
-    page_obj = paginator.get_page(page_number)  # Получаем объект страницы
-    context = {'paginator': paginator,  # Передаём пагинатор для доступа к метаданным
-        'page_number': page_number,'categories':ProductCategory.objects.all(),'products':page_obj,}
-    return render(request, "products/products.html",context)
+
+class commonView(TemplateView):
+    template_name = 'products/common.html'
+    def get_context_data(self, **kwargs):
+        context = super(commonView, self).get_context_data(**kwargs)
+        context['title'] = 'Neighbourhood'
+        return context
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
+    def get_queryset(self):
+        queryset = super(ProductListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context['title'] = 'Neighbourhood'
+        context['categories'] = ProductCategory.objects.all()
+        return context
+
+# def products(request,category_id=None):
+#     if category_id:
+#         products_list = Product.objects.filter(category_id=category_id)
+#     else:
+#         products_list = Product.objects.all()
+#     paginator = Paginator(products_list, 2)  # 2 товара на страницу
+#     page_number = request.GET.get('page')  # Получаем номер страницы из запроса
+#     page_obj = paginator.get_page(page_number)  # Получаем объект страницы
+#     context = {'paginator': paginator,  # Передаём пагинатор для доступа к метаданным
+#         'page_number': page_number,'categories':ProductCategory.objects.all(),'products':page_obj,}
+#     return render(request, "products/products.html",context)
 
 
 from django.shortcuts import redirect
