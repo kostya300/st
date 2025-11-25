23d4dd39f5a9cf93c6b8e2a61f045449e0deb800
ix0xid <Kostya.barnung@gmail.com>
2025-11-25 15:50:08 +0500

    nextcommit123

--- orders/models.py
+++ orders/models.py
@@ -20,11 +20,14 @@ class Order(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     email = models.EmailField(max_length=256)
-    address = models.CharField(max_length=11)
+    address = models.CharField(max_length=256)
     basket_history = models.JSONField(default=dict)
     created = models.DateTimeField(auto_now_add=True)
     status = models.SmallIntegerField(default=CREATED, choices=STATUSES)
-    initiator = models.ForeignKey(User, on_delete=models.CASCADE)
+    initiator = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
 
     def __str__(self):
-        return f'Заказ {self.first_name} {self.last_name}'
+        return f'Заказ №{self.id} от {self.first_name} {self.last_name}' if self.id else 'Новый заказ'
+
+
--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 import mimetypes
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", commonView.as_view(), name="common"),
